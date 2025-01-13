8787915b99f135e0db23a48741581d8ec4061752
snitkjaer <snitkjaer@gmail.com>
2025-01-13 12:33:26 +0100

    Fix rename_device

--- soti_mobicontrol_python/endpoints/device/device_rename.py
+++ soti_mobicontrol_python/endpoints/device/device_rename.py
@@ -1,7 +1,7 @@
 from ...SotiApiClient import SotiApiClient
 
 # Send action to a device
-async def send_action(client:SotiApiClient, device_id:str, new_name:str = None):
+async def rename_device(client:SotiApiClient, device_id:str, new_name:str = None):
     # /devices/{deviceId}/actions
     endpoint = f"/devices/{device_id}/actions"
 
